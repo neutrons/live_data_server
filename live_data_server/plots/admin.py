798045e8c31c42fd351e1b0faca6efc26d4e45c7
Doucet, Mathieu <doucetm@ornl.gov>
2016-06-30 14:01:10 -0400

    update admin option

--- live_data_server/plots/admin.py
+++ live_data_server/plots/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from plots.models import DataRun, Instrument, PlotData
 
 class PlotDataAdmin(admin.ModelAdmin):
+    readonly_fields=('data_run',)
     list_display = ('id', 'data_run', 'data_type', 'timestamp')
 
 admin.site.register(DataRun)
