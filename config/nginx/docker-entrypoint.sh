adbe9ad5885234f1b58e093b480669f343d95ebc
Jose Borreguero <borreguero@gmail.com>
2022-04-22 09:44:56 -0400

    add nginx draft files
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

---
+++ config/nginx/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# remove default nginx config
+rm -f /etc/nginx/conf.d/default.conf
+
+#https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+script=/usr/bin/wait-for-it.sh
+chmod +x $script
+bash -c "$script livedata:8000 --timeout=300"
