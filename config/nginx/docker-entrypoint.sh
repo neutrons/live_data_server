ac0a8dcf661679564b1f4fb28b27291fad761e27
Jose Borreguero <borreguero@gmail.com>
2022-04-28 21:31:58 -0400

    configuration for nginx container
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- config/nginx/docker-entrypoint.sh
+++ config/nginx/docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# remove default nginx config
+# remove default nginx config. Required step
 rm -f /etc/nginx/conf.d/default.conf
 
-#https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
-script=/usr/bin/wait-for-it.sh
-chmod +x $script
-bash -c "$script livedata:8000 --timeout=300"
+# https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+bash -c "/usr/bin/wait-for-it.sh livedata:8000 --timeout=300"
