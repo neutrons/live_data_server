885e6d6248530f8fe48897f25f7c865b337a4b38
Ross Whitfield <whitfieldre1@ornl.gov>
2024-06-19 15:54:04 +1000

    Add code coverage

---
+++ docker-entrypoint-coverage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+# wait for database
+until PGPASSWORD=${DATABASE_PASS} psql -h "${DATABASE_HOST}" -U "${DATABASE_USER}" -d "${DATABASE_NAME}" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+cd /var/www/livedata/app
+
+# collect static files
+python manage.py collectstatic --noinput
+
+# migrate django models
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# create superuser
+#echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_USERNAME}@example.com', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
+
+# Create the webcache
+python manage.py createcachetable webcache
+python manage.py ensure_adminuser --username=${DJANGO_SUPERUSER_USERNAME} --email='workflow@example.com' --password=${DJANGO_SUPERUSER_PASSWORD}
+
+# run application with code coverage
+coverage run -m gunicorn live_data_server.wsgi:application -b :8000
