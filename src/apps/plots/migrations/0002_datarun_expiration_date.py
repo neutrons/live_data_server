e2b5025de50fb74d2f804f128af3cb00ea11d6e9
glass-ships <glass.ships@outlook.com>
2024-09-17 14:47:25 -0400

    add instruments enum, use that to differentiate

--- src/apps/plots/migrations/0002_datarun_expiration_date.py
+++ src/apps/plots/migrations/0002_datarun_expiration_date.py
@@ -5,14 +5,16 @@ import datetime
 from django.conf import settings
 from django.db import migrations, models
 
+from config.instruments import Instruments
+
 
 def set_expiration_date(apps, _):
     DataRun = apps.get_model("plots", "DataRun")
     for run in DataRun.objects.all():
-        if run.run_number == 0:
-            run.expiration_date = run.created_on + datetime.timedelta(days=365 * 100)
-        else:
+        if Instruments.has_value(run.instrument.name):
             run.expiration_date = run.created_on + datetime.timedelta(days=settings.LIVE_PLOT_EXPIRATION_TIME)
+        else:
+            run.expiration_date = run.created_on + datetime.timedelta(days=365 * 100)
         run.save()
 
 
